8928e109e2245652d1abe7bd9177b133912d20c8
Jerome Sta. Maria <stamaria.jerome@gmail.com>
2016-07-16 14:29:05 +0800

    fixed server listen

--- app.js
+++ app.js
@@ -68,6 +68,4 @@ app.use(commentRoutes);
 app.use(userRoutes);
 
 
-app.listen('3000', function() {
-  console.log('Server started at port 3000..');
-});
+app.listen(process.env.PORT, process.env.IP);
